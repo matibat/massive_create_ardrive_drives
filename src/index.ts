ca86924b4edfccde9db679cc593b8db5b17f51f3
Mati <matibat@gmail.com>
2023-09-05 19:59:25 -0300

    feat(massive create drives): wip tool PE-4502

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,46 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+import { ArFSEntityData, arDriveFactory, readJWKFile } from 'ardrive-core-js';
+import { program } from 'commander';
 
-// eslint-disable-next-line
-export class DefaultClient {}
+
+// massive_create_ardrive_drives --wallet "$PATH_TO_MY_WALLET" --prefix "my_prefix" --number 100
+
+program
+  .command('run')
+  .description('Create ArDrive drives in mass')
+  .option('-d, --dry-run', 'Dry run')
+  .requiredOption('-w, --wallet <path>', 'Path to wallet file')
+  .requiredOption('-p, --prefix <prefix>', 'Prefix for the drive names')
+  .requiredOption('-n, --number <number>', 'Number of drives to create')
+  .action(async (_, options) => {
+    const walletPath = options.wallet;
+    const prefix = options.prefix;
+    const number = parseInt(options.number);
+    const dryRun = options.dryRun;
+
+    console.log('Creating ArDrive drives... ');
+
+    // Read wallet from file
+    const myWallet = readJWKFile(walletPath);
+
+    // Construct ArDrive class
+    const arDrive = arDriveFactory({ wallet: myWallet, dryRun });
+
+    const createdDrives: Array<ArFSEntityData> = [];
+
+    for (let i = 0; i < number; i++) {
+      // Create a public drive and its root folder
+      const createDriveResult = await arDrive.createPublicDrive({
+        driveName: `${prefix}-${i}`,
+        
+      });
+
+      createdDrives.push(...createDriveResult.created);
+    }
+
+    console.log(createdDrives);
+  });
+
+program.parse(process.argv);
