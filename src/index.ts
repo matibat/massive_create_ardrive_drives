0f67f8ec801134a628db2f077a8b40f44a201309
Mati <matibat@gmail.com>
2023-09-06 15:46:45 -0300

    feat(massive create drives): let it use the turbo service PE-4502

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { ArFSEntityData, arDriveFactory, readJWKFile } from 'ardrive-core-js';
+import { ArFSEntityData, TurboSettings, arDriveFactory, readJWKFile } from 'ardrive-core-js';
 import { program } from 'commander';
 
 
@@ -24,6 +24,7 @@ program
   .command('run')
   .description('Create ArDrive drives in mass')
   .option('-d, --dry-run', 'Dry run')
+  .option('-t, --turbo', 'Use Turbo serivce')
   .requiredOption('-w, --wallet <path>', 'Path to wallet file')
   .requiredOption('-p, --prefix <prefix>', 'Prefix for the drive names')
   .requiredOption('-n, --number <number>', 'Number of drives to create')
@@ -32,16 +33,16 @@ program
     const prefix = options.prefix;
     const number = parseInt(options.number);
     const dryRun = options.dryRun;
+    const turbo = options.turbo;
 
-    Headers;
 
     console.log('Creating ArDrive drives... ');
 
-    // Read wallet from file
+    const turboSettings: TurboSettings | undefined = turbo ? {
+      turboUrl: new URL('https://payment.ardrive.dev')
+    } : undefined;
     const myWallet = readJWKFile(walletPath);
-
-    // Construct ArDrive class
-    const arDrive = arDriveFactory({ wallet: myWallet, dryRun });
+    const arDrive = arDriveFactory({ wallet: myWallet, dryRun, turboSettings });
 
     const createdDrives: Array<ArFSEntityData> = [];
 
